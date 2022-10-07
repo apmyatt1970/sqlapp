9b3ac0143e63eaf3d9b2a4f8eae254164e7cedc1
Administrator <Administrator@ANT-LAPTOP>
2022-10-07 21:35:35 +0100

    Add project files.

---
+++ sqlapp/Services/ProductService.cs
@@ -0,0 +1,54 @@
+﻿using sqlapp.Models;
+using System.Data.SqlClient;
+
+namespace sqlapp.Services
+{
+    public class ProductService
+    {
+        private static string db_source = "appserver51.database.windows.net";
+        private static string db_user = "sqladmin";
+        private static string db_password = "shark_sn0t";
+        private static string db_database = "appdb";
+
+        private SqlConnection GetConnection()
+        {
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+            builder.DataSource = db_source;
+            builder.UserID = db_user;
+            builder.Password = db_password;
+            builder.InitialCatalog = db_database;
+
+            return new SqlConnection(builder.ConnectionString);
+        }
+
+        public List<Product> GetProducts()
+        {
+            List<Product> productList = new List<Product>();
+            string statement = "SELECT ProductID, ProductName, Quantity FROM Products";
+
+            using (SqlConnection conn = GetConnection())
+            {
+                conn.Open();
+                using (SqlCommand cmd = new SqlCommand(statement, conn))
+                {
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Product product = new Product()
+                            {
+                                ProductID = reader.GetInt32(0),
+                                ProductName = reader.GetString(1),
+                                Quantity = reader.GetInt32(2)
+                            };
+
+                            productList.Add(product);
+                        }
+                    }
+                }
+            }
+
+            return productList;
+        }
+    }
+}
